a610c0381072e06445697a81df7c8c64d90b4889
vitaly.kireev <vitaly.kireev@gmail.com>
2025-01-28 20:13:49 +0300

    build new version

---
+++ assets/use-quasar-BiL6YFO2.js
@@ -0,0 +1,1 @@
+import{i as a,bJ as r}from"./index-BGy2oowu.js";function u(){return a(r)}export{u};
--- assets/use-quasar-Plz2atnA.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bI as r}from"./index-BD0aB69o.js";function u(){return a(r)}export{u};
