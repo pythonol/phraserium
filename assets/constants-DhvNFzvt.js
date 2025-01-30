cd140a894e42b94cd6f8750934fb17956726d2bc
vitaly.kireev <vitaly.kireev@gmail.com>
2025-01-30 12:55:55 +0300

    build new version

---
+++ assets/constants-DhvNFzvt.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-D1nlLIt7.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
---
+++ assets/use-quasar-DW4_Zn-T.js
@@ -0,0 +1,1 @@
+import{i as a,bF as r}from"./index-D1nlLIt7.js";function u(){return a(r)}export{u};
