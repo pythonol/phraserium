841984a34b537b29e4b11815f127f46145aa40f4
vitaly.kireev <vitaly.kireev@gmail.com>
2025-01-31 18:45:41 +0300

    build new version

---
+++ assets/constants-Dqg5pDhL.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-DQRA6LH0.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
---
+++ assets/use-quasar-DeWG_FhQ.js
@@ -0,0 +1,1 @@
+import{i as a,bF as r}from"./index-DQRA6LH0.js";function u(){return a(r)}export{u};
