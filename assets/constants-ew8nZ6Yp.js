4520ffea5ee4caa12118b643ff4acddc3bf53795
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-01 01:19:25 +0300

    build new version

--- assets/constants-ew8nZ6Yp.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-CTVi1yMK.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
--- assets/use-quasar-Cz9OVUwR.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bF as r}from"./index-CTVi1yMK.js";function u(){return a(r)}export{u};
