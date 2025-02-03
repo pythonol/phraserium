c26827a6642025eb40762fa09920a81a63457804
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-04 01:17:45 +0300

    build new version

--- assets/constants-6BK2GwnR.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-CW9XW_hH.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
--- assets/use-quasar-DBGu4LZx.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bF as r}from"./index-CW9XW_hH.js";function u(){return a(r)}export{u};
