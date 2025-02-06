3d2716975133536dc322363db808743df5775fd1
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-06 17:21:27 +0300

    build new version

---
+++ assets/constants-CsBtH51M.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CikNEMW4.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
---
+++ assets/use-quasar-CA4wHeH5.js
@@ -0,0 +1,1 @@
+import{i as a,bL as r}from"./index-CikNEMW4.js";function u(){return a(r)}export{u};
