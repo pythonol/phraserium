0ff3ac7b2407a8221cbccd8ca04d45dfabfbb56a
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-12 15:21:21 +0300

    build new version

--- pwa/assets/constants-9_BY3YCw.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-_W0DJGDh.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
---
+++ pwa/assets/constants-L6dnds-X.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-kJQuNSRz.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
