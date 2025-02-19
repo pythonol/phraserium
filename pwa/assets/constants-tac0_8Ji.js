9ad26b08a706d489f6aac37352ee78709ecd53e7
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-20 02:46:00 +0300

    build new version

---
+++ pwa/assets/constants-tac0_8Ji.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-_4E9V9sX.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
---
+++ pwa/assets/use-quasar-C5kTFe9F.js
@@ -0,0 +1,1 @@
+import{i as a,bG as r}from"./index-_4E9V9sX.js";function u(){return a(r)}export{u};
