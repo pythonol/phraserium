70632377cb2514e9f516921f88327813cbcda38c
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-20 01:24:27 +0300

    build new version

--- pwa/assets/constants-ClT_mzp-.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-Dvb6LP9V.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
--- pwa/assets/use-quasar-7drXMM5g.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bG as r}from"./index-Dvb6LP9V.js";function u(){return a(r)}export{u};
