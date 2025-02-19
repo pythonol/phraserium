a1c377340125518b13ad57023796d4d66fd0721f
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-19 22:59:29 +0300

    build new version

--- pwa/assets/constants-CW6jNGgP.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-DtiRVVaF.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
---
+++ pwa/assets/use-quasar-7drXMM5g.js
@@ -0,0 +1,1 @@
+import{i as a,bG as r}from"./index-Dvb6LP9V.js";function u(){return a(r)}export{u};
