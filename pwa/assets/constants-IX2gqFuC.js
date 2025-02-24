826fa300f6f304d86fd9eb9ac440b7aeb30021aa
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-24 11:09:54 +0300

    build new version

---
+++ pwa/assets/constants-IX2gqFuC.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-Ci_zo6Z1.js";const n=({app:t})=>{t.config.globalProperties.$constants=o};export{n as default};
---
+++ pwa/assets/use-quasar-DHZf4eer.js
@@ -0,0 +1,1 @@
+import{i as a,bH as r}from"./index-Ci_zo6Z1.js";function u(){return a(r)}export{u};
