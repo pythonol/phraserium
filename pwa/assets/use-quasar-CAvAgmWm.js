7111a2821792f59e975465daaac9668119fb42b5
vitaly.kireev <vitaly.kireev@gmail.com>
2025-02-26 11:04:25 +0300

    build new version

---
+++ pwa/assets/use-quasar-CAvAgmWm.js
@@ -0,0 +1,1 @@
+import{i as a,bH as r}from"./index-CQ3QZ9kr.js";function u(){return a(r)}export{u};
--- pwa/assets/use-quasar-D7DPop1c.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bH as r}from"./index-CsfDGaYH.js";function u(){return a(r)}export{u};
